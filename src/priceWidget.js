76c5d8db4644a7a57f58d4ea6f8d9fb551db5bfc
hidude562 <poopybutface8@gmail.com>
2023-05-11 19:37:04 -0400

    days fix

--- src/priceWidget.js
+++ src/priceWidget.js
@@ -1,15 +1,26 @@
 var price = 0;
 
-function newCostItem(val, otherText) {
-	newJson = JSON.parse(val)
-	price+=parseFloat(newJson.price)
-	
-	// Credit: http://www.java2s.com/Tutorials/Javascript/Javascript_Element_How_to/UL/Create_new_LI_element_and_append_to_UL.htm
-	el = document.createElement('li');
-    el.innerHTML = newJson.name + ": $" + newJson.price + otherText;
-    document.getElementById('costItems').appendChild(el);
-	
-	document.getElementById('totalPrice').innerHTML = "$" + price
+function newCostItem(val, otherText, mode) {
+	// Mode 0 acts nightly for price
+	try {
+		newJson = JSON.parse(val)
+		
+		if(mode == 0) {
+			var days = parseFloat(sessionStorage.days)
+			otherText += " ($" + (days * parseFloat(newJson.price)) + ")"
+			price+=(days * parseFloat(newJson.price))
+		} else {
+			price+=parseFloat(newJson.price)
+		}
+		
+		// Credit: http://www.java2s.com/Tutorials/Javascript/Javascript_Element_How_to/UL/Create_new_LI_element_and_append_to_UL.htm
+		el = document.createElement('li');
+		el.innerHTML = newJson.name + ": $" + newJson.price + otherText;
+		document.getElementById('costItems').appendChild(el);
+		document.getElementById('totalPrice').innerHTML = "$" + price
+	} catch(e) {
+		
+	}
 }
 
 function clearCostItems() {
@@ -20,11 +31,12 @@ function clearCostItems() {
 
 function updateDisplays() {
 	clearCostItems()
-	newCostItem(sessionStorage.ship, " per night");
+	newCostItem(sessionStorage.ship, " per night", 0);
+	newCostItem(sessionStorage.destination, " additional per night", 0);
 }
 
 updateDisplays()
 
 var intervalId = window.setInterval(function () {
-	updateDisplays()
-}, 500);
+	  updateDisplays()
+}, 100);
