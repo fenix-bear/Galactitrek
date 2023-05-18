8fd519a2d715ed3dc0ac183a8baff6dcef4bd820
fenix-bear <benttonp@protonmail.com>
2023-05-18 13:57:01 -0400

    more unfinished css keyframes

--- src/logo.js
+++ src/logo.js
@@ -1,13 +1,35 @@
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+var box;
+var target;
 
-async function main(){
-    var svgElement = document.getElementById("text113");
-    while(true){
-        console.log(svgElement.parentElement.parentElement.parentElement.scrollHeight);
-        await sleep(1000)
+function updateAnimation() {
+  box = document.querySelector('.mainlogo');
+  target = document.querySelector('.navlogo');
+  if(target != null){
+  console.log(box)
+  console.log(target)
+  const targetRect = target.getBoundingClientRect();
+  const targetX = targetRect.left + targetRect.width / 2 - box.offsetWidth / 2;
+  const targetY = targetRect.top + targetRect.height / 2 - box.offsetHeight / 2;
+  console.log(targetRect.left, targetRect.width, box.offsetWidth);
+  const style = `
+    @keyframes minimizelogo {
+      0% {
+        transform: translate(0, 0);
+        height: 8vw;
+      }
+      50% {
+        transform: translate(${targetX}px, ${targetY}px);
+        height: 20px;
+      }
+      100% {
+      }
     }
+  `;
+
+  const animationStyle = document.createElement("style");
+  animationStyle.innerHTML = style;
+  document.head.appendChild(animationStyle);}
 }
 
-main()
+window.addEventListener("scroll", updateAnimation);
+updateAnimation();
