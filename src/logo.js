ea5d80b31fab4d0c2ff2f1d25d57f0a5a61d348b
fenix-bear <benttonp@protonmail.com>
2023-05-19 10:48:11 -0400

    css transition funcitonal

--- src/logo.js
+++ src/logo.js
@@ -1,35 +1,18 @@
-var box;
-var target;
+var navlogo;
 
-function updateAnimation() {
-  box = document.querySelector('.mainlogo');
-  target = document.querySelector('.navlogo');
-  if(target != null){
-  console.log(box)
-  console.log(target)
-  const targetRect = target.getBoundingClientRect();
-  const targetX = targetRect.left + targetRect.width / 2 - box.offsetWidth / 2;
-  const targetY = targetRect.top + targetRect.height / 2 - box.offsetHeight / 2;
-  console.log(targetRect.left, targetRect.width, box.offsetWidth);
-  const style = `
-    @keyframes minimizelogo {
-      0% {
-        transform: translate(0, 0);
-        height: 8vw;
-      }
-      50% {
-        transform: translate(${targetX}px, ${targetY}px);
-        height: 20px;
-      }
-      100% {
-      }
+const observer = new IntersectionObserver((entries) => {
+  navlogo = document.querySelector(".navlogo");
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (!entry.isIntersecting) {
+      navlogo.classList.add("logovisible");
+    } else {
+      navlogo.classList.remove("logovisible");
     }
-  `;
+  })
+});
 
-  const animationStyle = document.createElement("style");
-  animationStyle.innerHTML = style;
-  document.head.appendChild(animationStyle);}
-}
+console.log(navlogo);
 
-window.addEventListener("scroll", updateAnimation);
-updateAnimation();
+const hiddenElements = document.querySelectorAll(".mainlogo");
+hiddenElements.forEach((el) => observer.observe(el));
