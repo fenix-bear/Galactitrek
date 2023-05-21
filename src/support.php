8f77e9e22b5596f32189b30b4443bb6907a71c5d
Pyro <79991107+Pyro569@users.noreply.github.com>
2023-05-21 19:30:48 -0400

    Update support.php

--- src/support.php
+++ src/support.php
@@ -1,17 +1,17 @@
 <?php ob_start(); ?>
-    <div class="d-md-flex flex-md-equal my-md-3 pl-md-3">
-        <div class="mainContent mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-left text-white overflow-hidden">
-            <div class="my-3 py-3 row">
-                <div>
-                    <h2 class="display-5 font-weight-light">Support:</h2>
-                    <p class="lead font-weight-medium">If you are need in support regarding a booking for Galactitrek please send e-mails to support@galactitrek.com, calls to (444)4444-444 and faxes to (444)4444-445. If you are in need of a direct contact with a manager, please skype call (444)444-4446.</p>
-                    <h2 class="display-5 font-weight-light"></h2>
-                </div>
-                <div class="col-sm">
-                </div>
-
+<div class="d-md-flex flex-md-equal my-md-3 pl-md-3">
+    <div class="mainContent mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-left text-white overflow-hidden">
+        <div class="my-3 py-3 row">
+            <div>
+                <h2 class="display-5 font-weight-light">Support:</h2>
+                <p class="lead font-weight-medium">If you are need in support regarding a booking for Galactitrek please send e-mails to support@galactitrek.com, calls to (444)4444-444 and faxes to (444)4444-445. If you are in need of a direct contact with a manager, please skype call (444)4444-446.</p>
+                <h2 class="display-5 font-weight-light"></h2>
             </div>
+            <div class="col-sm">
+            </div>
+
         </div>
     </div>
+</div>
 <?php $content = ob_get_clean();
 include "parent.php"; ?>
