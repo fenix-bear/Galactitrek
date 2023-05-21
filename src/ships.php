a25de2c8ab4d26240fa0b3453392583c87bbfd13
hidude562 <poopybutface8@gmail.com>
2023-05-21 15:13:37 -0400

    Replace placeholder text

--- src/ships.php
+++ src/ships.php
@@ -8,10 +8,10 @@
     <p class="lead">At Galactitrek, we bring the universe to you. That's right, unlike other companies, our warp drive uses patented technology that works by bringing the universe to you. This means our ships can go faster than any other warp drive. For example, our fastest ship <a href="ships.php#AllureOfTheStars">Allure of the Stars</a> can travel at about 1 million times the speed of light.</p>
     
     <h2 class="display-5 font-weight-light">Comfort</h2>
-    <p class="lead gradient2">At Galactitrek, we bring the universe to you. That's right, unlike other companies, our warp drive uses patented technology that works by bringing the universe to you. This means our ships can go faster than any other warp drive. For example, our fastest ship <a href="ships.php#AllureOfTheStars">Allure of the Stars</a> can travel at about 1 million times the speed of light.</p>
+    <p class="lead gradient2">Also, at Galactitrek, we provide the highest quality comfort so that you can enjoy your interstellar voyage in maximum relaxation. We also have 24/7 personal AI assistants so that you can have every need taken care of.</p>
     
     <h2 class="display-5 font-weight-light">Price</h2>
-    <p class="lead">At Galactitrek, we bring the universe to you. That's right, unlike other companies, our warp drive uses patented technology that works by bringing the universe to you. This means our ships can go faster than any other warp drive. For example, our fastest ship <a href="ships.php#AllureOfTheStars">Allure of the Stars</a> can travel at about 1 million times the speed of light.</p>
+    <p class="lead">Lastly, at Galactitrek, we offer some of the best pricing in the world for our ships. Unlike Astrotours or SpaceX, we actually sell our rooms at a slight loss. We do this so that everyone can get a chance at experiencing space.</p>
     
     <h2 class="display-5 font-weight-light" style="color: yellow">Personal Ship</h2>
       <p class="lead gradient2">If you can afford it, this will certainly be the best option for you. You can plot your own course, and make your own specialized adventure with this ship. Being the size of a medium-sized apartment building, this ship is the ultimate experience for anyone looking to explore the universe.</p>
